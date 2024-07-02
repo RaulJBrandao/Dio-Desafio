ed9925bbbe70a241ca70a527d91672c1e21af92b
Raul <rauloficial8@gmail.com>
2024-07-02 17:58:22 -0300

    1.0

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,19 @@
-public class ContaTerminal {
+ import java.util.Scanner;
+ public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+        String agencia, nomeCliente;
+        float saldo = 237.48F;
+
+        System.out.print("Por favor, insira o número de sua agência: ");
+        agencia = scanner.next();
+        System.out.print("Por favor, insira o seu nome: ");
+        nomeCliente = scanner.next();
+        System.out.print("Por favor, insira o numero da conta: ");
+        numero = scanner.nextInt();
         
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta número "+numero+" e seu saldo de "+saldo+" já está disponível para saque.");
     }
 }
